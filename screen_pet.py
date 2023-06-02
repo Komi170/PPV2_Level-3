97ffc310a43b7665cab9b8bac60f0089e1fbad99
Komi170 <135326701+Komi170@users.noreply.github.com>
2023-06-02 17:25:29 +0530

    Add files via upload

--- screen_pet.py
+++ screen_pet.py
@@ -21,9 +21,7 @@ c.configure(bg='dark blue',highlightthickness=0)
 c.body_color = 'SkyBlue1'
 body = c.create_oval(35, 20, 365, 350, outline=c.body_color, fill=c.body_color)
 ear_left = c.create_polygon(75, 80, 75, 10, 165, 70, outline=c.body_color, fill=c.body_color)
-ear_right = c.create_polygon(255, 45, 325, 10, 320, 70, outline=c.body_color, \
-                             fill=c.body_color)
-
+ear_right = c.create_polygon(255, 45, 325, 10, 320, 70, outline=c.body_color, fill=c.body_color)
 foot_left = c.create_oval(65, 320, 145, 360, outline=c.body_color, fill= c.body_color)
 foot_right = c.create_oval(250, 320, 330, 360, outline=c.body_color, fill= c.body_color)
 eye_left = c.create_oval(130, 110, 160, 170, outline='black', fill='white')
@@ -35,7 +33,7 @@ mouth_normal = c.create_line(170, 250, 200, 272, 230, 250, smooth=1, width=2, st
 mouth_happy = c.create_line(170, 250, 200, 282, 230, 250, smooth=1, width=2, state=HIDDEN)
 mouth_sad = c.create_line(170, 250, 200, 232, 230, 250, smooth=1, width=2, state=HIDDEN)
 cheek_left = c.create_oval(70, 180, 120, 230, outline='pink', fill='pink', state=HIDDEN)
-cheek_right = c.create_oval(280, 180, 330, 230, outline='pink', fill='pink', state=HIDDEN)
+cheek_right = c.create_oval(280, 180, 330, 230, outline='pink', fill='pink', state=HIDDEN)  
 
 c.pack()
 
@@ -58,6 +56,3 @@ def hide_happy(event):
 c.bind('<Motion>', show_happy)
 c.bind('<Leave>', hide_happy)
 root.mainloop()
-
-
-
